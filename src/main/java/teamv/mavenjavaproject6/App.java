98e9cd3e78ce5adc5ff53b680310803a6147626f
fayaz9985 <f6133942@gmail.com>
2025-01-17 20:32:41 +0530

    first commit

---
+++ src/main/java/teamv/mavenjavaproject6/App.java
@@ -0,0 +1,13 @@
+package teamv.mavenjavaproject6;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
